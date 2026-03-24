7f33a2fb232fe3e30441866211f1da47e9d1284e
Victor Kauã <victorelezeu@gmail.com>
2026-03-24 00:49:04 -0300

    Exercicio 5

---
+++ Categorias/src/test/java/com/Gerenciamento_Categorias/Categorias/CategoriasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Gerenciamento_Categorias.Categorias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoriasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
